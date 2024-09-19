59805dcf7c53786955fb4091f5e974112a0406f4
Fernand Diaz <fernand.diaz1708@gmail.com>
2024-09-19 13:05:12 -0300

    feat(database/migrations): Add foreign key for user, property type and status enums, area column with decimal data type, rooms column, description column
       This commit adds a foreign key for the User model to the properties table, as well as columns for property type, status, area, rooms, and description. The area column is now of decimal data type, while the other new columns are text or enum types. These changes improve the structure and flexibility of the properties table in the application's database.

--- database/migrations/2024_09_19_144134_create_properties_table.php
+++ database/migrations/2024_09_19_144134_create_properties_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Enums\PropertyStatus;
+use App\Enums\PropertyType;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,14 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->string('address');
+            $table->enum('type', PropertyType::toArray());
+            $table->decimal(column:'area', total:5, places:1);
+            $table->tinyInteger('rooms');
+            $table->text('description');
+            $table->enum('status', PropertyStatus::toArray());
             $table->timestamps();
         });
     }
