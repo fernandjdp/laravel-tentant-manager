3b18dd3c30d944289c6e9f90d08e0ac5c8a71516
Fernand Diaz <fernand.diaz1708@gmail.com>
2024-09-19 13:21:03 -0300

    feat: Add mass assignable attributes, casts and relationships to Property model
    Added mass assignable attributes for user_id, name, address, type, area, rooms, description, and status. Implemented casting for the type and status attributes using the PropertyType and PropertyStatus enums respectively. Also added a relationship method for the tenant user. This change improves the structure of the Property model by making it more flexible and easier to work with.

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,10 +2,51 @@
 
 namespace App\Models;
 
+use App\Enums\PropertyStatus;
+use App\Enums\PropertyType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'type',
+        'area',
+        'rooms',
+        'description',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'type' => PropertyType::class,
+            'status' => PropertyStatus::class,
+        ];
+    }
+
+    /**                                                                                                                   **
+     * -------------------------------------------------- Relationships -------------------------------------------------- *
+     *                                                                                                                     */
+
+     // Tenant user
+    public function tenant()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
